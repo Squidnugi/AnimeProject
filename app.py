cb45cfbb5e211b74a7df937baa99c47a7762c540
henrymcconville <henrymcconville@exe-coll.ac.uk>
2022-05-06 10:26:25 +0100

    Commit Message

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ import sqlite3
 print("http://localhost:5000/")
 app = Flask(__name__)
 
-def get_id_user(type):
+def get_id_user():
+    users = ''
     name = request.cookies.get('user')
     with sqlite3.connect('identifier.sqlite') as con:
         cur = con.cursor()
-
         cur.execute('SELECT username, ID FROM users')
 
         rows = cur.fetchall()
@@ -17,29 +17,17 @@ def get_id_user(type):
             if i[0] == name:
                 users = i[0]
                 ID = i[1]
-    if type:
-        return users
-    else:
-        return ID
+    return ID, users, name
 
 @app.route('/', methods = ['POST', 'GET'])
 def main():
-    users = ''
     ID = 0
     values = []
-    name = request.cookies.get('user')
-    with sqlite3.connect('identifier.sqlite') as con:
-        cur = con.cursor()
-
-        cur.execute('SELECT username, ID FROM users')
-
-        rows = cur.fetchall()
-
-        for i in rows:
-            if i[0] == name:
-                users = i[0]
-                ID = i[1]
-    if name == users:
+    temp = get_id_user()
+    name = temp[2]
+    user = temp[1]
+    ID = temp[0]
+    if name == user:
         with sqlite3.connect('identifier.sqlite') as con:
             cur2 = con.cursor()
 
@@ -57,7 +45,7 @@ def main():
                     temp['vol_max'] = i[4]
                     temp['img'] = i[5]
                     values.append(temp)
-        return render_template('index.html', name=name, table=values)
+        return render_template('index.html', name=user, table=values)
     else:
         return render_template('welcome.html')
 
@@ -71,7 +59,6 @@ def sign_up():
         temp.append(password)
         with sqlite3.connect("identifier.sqlite") as con:
             cur = con.cursor()
-            print('sup')
             cur.execute("INSERT INTO users (username, password) VALUES (?, ?)", temp)
             con.commit()
         resp = make_response(redirect(url_for('main')))
@@ -86,7 +73,6 @@ def login():
         logged = False
         user = request.form['username']
         password = request.form['password']
-        print('hi')
         with sqlite3.connect("identifier.sqlite") as con:
             cur = con.cursor()
 
@@ -129,16 +115,13 @@ def add():
             vol_max = int(vol_max)
         if img == '':
             img == 'Blank'
-        ID = get_id_user(False)
+        temp = get_id_user()
+        ID = temp[0]
         with sqlite3.connect("identifier.sqlite") as con:
             cur = con.cursor()
 
             cur.execute('INSERT INTO manga_db (user_ID, name, volumes, volumes_max, img) VALUES (?, ?, ?, ?, ?)', (ID, name, vol, vol_max, img))
             con.commit()
-        print(name, type(name))
-        print(img, type(name))
-        print(vol, type(vol))
-        print(vol_max, type(vol_max))
         return redirect(url_for('main'))
     else:
         return render_template('add.html')
@@ -151,28 +134,20 @@ def edit():
         vol = int(request.form['vol'])
         vol_max = request.form['vol_max']
         ID = request.form['ID']
-        print(vol_max)
         if vol_max == '':
             vol_max = 0
         else:
             vol_max = int(vol_max)
         if img == '':
             img == 'Blank'
-        print(manga, vol, vol_max, img, ID)
-        print(type(manga), type(vol), type(vol_max), type(img), type(ID))
         with sqlite3.connect("identifier.sqlite") as con:
             cur = con.cursor()
 
             cur.execute('UPDATE manga_db SET name = ?, volumes = ?, volumes_max = ?, img = ? WHERE ID = ?', (manga, vol, vol_max, img, ID))
             con.commit()
-        print(manga, type(manga))
-        print(img, type(manga))
-        print(vol, type(vol))
-        print(vol_max, type(vol_max))
         return redirect(url_for('main'))
     else:
         ID = int(request.args.get('test'))
-        print(type(ID))
         values = {}
         with sqlite3.connect('identifier.sqlite') as con:
             cur = con.cursor()
@@ -189,13 +164,11 @@ def edit():
                     values['vol'] = i[3]
                     values['vol_max'] = i[4]
                     values['img'] = i[5]
-        print(values)
         return render_template('edit.html', info=values)
 
 @app.route('/delete', methods = ['GET', 'POST'])
 def delete():
     if request.method == 'POST':
-        print('test')
         ID = request.form['dele']
         with sqlite3.connect("identifier.sqlite") as con:
             cur = con.cursor()
